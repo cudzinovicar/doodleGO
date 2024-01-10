e6b7671f093a3a82e13dbd8b125fe8178c434ade
cudzinovicar <cudzinovicar@gmail.com>
2024-01-09 20:59:07 -0600

    nav bar ui fix

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,8 +12,8 @@
                     </svg>
                 </button>
             </div>                
-            <div v-show="showDiv" class="items-center justify-between  w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <div v-bind:class="{ 'hidden': !showDiv, 'flex': showDiv }" class="lg:flex lg:flex-grow items-center items-center justify-between  w-full md:flex md:w-auto md:order-1"  id="navbar-sticky">
+                <ul class="flex flex-col p-4 lg:flex-row list-none ml-auto md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
                         <router-link :to="{ name: 'home' }" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >
                             Home
