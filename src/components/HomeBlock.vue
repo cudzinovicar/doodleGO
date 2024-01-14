c9235a28bc9f42c397cdb339bab250533de67f86
cudzinovicar <cudzinovicar@gmail.com>
2024-01-14 11:35:34 -0600

    image and alt fix

--- src/components/HomeBlock.vue
+++ src/components/HomeBlock.vue
@@ -2,7 +2,7 @@
     <div ref="main" :class="getBackgroundGrad()" class=" mx-auto animate-fade-up animate-ease-out animate-delay-100  box-border text-center h-fit w-fit break-after-auto p-5 my-36 md:my-32 md:flex" >
         <div class ="md:w-1/2">
             <h1 class="pb-5 font-extrabold">{{ title }}</h1>
-            <img class="fill-black" :src="imgPath" alt="`${alt}`"/>
+            <img class="fill-black" :src="imgPath" :alt="alt"/>
         </div>
         
         <p class=" flex font-mono items-center justify-center md:w-1/2 whitespace-pre-line p-5">{{ desc }}</p>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,18 @@
     <div class="w-full overflow-auto justify-center align-middle no-scrollbar">
         <div>
             <HomeBlock class="bg-gradient-to-br from-red-500 via-pink-400 to-purple-600"
-            color1="red" color2="purple" title="Ready" desc="A widely available web app game for desktop and mobile use." imgPath="./src/assets/ready.svg" alt="Multiple computer devices including a desktop monitor, tablet, and portable phone"/>
+            color1="red" color2="purple" title="Ready" desc="A widely available web app game for desktop and mobile use." imgPath="./images/ready.svg" alt="Multiple computer devices including a desktop monitor, tablet, and portable phone"/>
 
         </div>
         <div>
             <HomeBlock class="bg-gradient-to-br from-yellow-500 via-amber-400  to-orange-600" color1="yellow"  color2="blue" title="Set" desc="Easy to make sets for any theme or topic imaginable. 
             Users will consectively draw the items in the set.
-            Publish for anyone to use." imgPath="./src/assets/sets.svg" alt=""/>
+            Publish for anyone to use." imgPath="./images/sets.svg" alt=""/>
 
         </div>
         <div>
             <HomeBlock class="bg-gradient-to-br from-lime-500 via-green-400  to-blue-600" color1="lime" color2="orange" title="GO" desc="Customize the drawing experience to any preference.
-            Upload the GO results to social media or the integrated platform to share and interact with other users and find more Sets." imgPath="./src/assets/go.svg" alt=""/>
+            Upload the GO results to social media or the integrated platform to share and interact with other users and find more Sets." imgPath="./images/go.svg" alt=""/>
 
         </div>
 
