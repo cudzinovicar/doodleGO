7c4dfb7435ed7c0546e857b281d36cdb9097b108
cudzinovicar <cudzinovicar@gmail.com>
2024-01-20 16:13:03 -0600

    component warning fix

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -2,9 +2,10 @@
    
   <div class="text-black dark:text-white mx-auto items-baseline pt-50 items-center
    justify-center flex-grow">
-   <navbar class="flex-1"/>
-    <router-view class="flex-1"/>
+   <Navbar class="flex-1"/>
+    
   </div>
+  <router-view />
  
 </template>
   
