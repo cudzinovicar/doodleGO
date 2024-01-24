1b7c9590b83412d7b2c39b315e181f99a20827a2
cudzinovicar <80971983+cudzinovicar@users.noreply.github.com>
2024-01-24 12:14:15 -0600

    Variable setup

---
+++ src/go/config.js
@@ -0,0 +1,9 @@
+import { reactive } from 'vue'
+
+export const config = reactive({
+    color : "#000000",
+    size : 5,
+    tool : 0,
+    
+
+})
--- src/views/SandboxTest.vue
+++ src/views/SandboxTest.vue
@@ -1,3 +1,28 @@
 <template>
-    Coming Soon!
+    <div ref="Prompter">
+
+    </div>
+
+    <vue-drawing-canvas ref="VueCanvasDrawing" 
+        width=width height = height />
 </template>
+
+
+<script>
+    import VueDrawingCanvas from "vue-drawing-canvas";
+    import {config} from "../go/config.js"
+
+    export default{
+        name: "GoGame",
+        props:{
+            palette: String,
+            tools: String,
+            width: Number,
+            height: Number,
+            time: Number
+        },
+        components: {
+            VueDrawingCanvas,
+        }
+    };
+</script>
