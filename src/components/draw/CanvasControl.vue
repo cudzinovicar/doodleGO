7943c2848068f6d19a566fb8ccee5d086be8792a
cudzinovicar <80971983+cudzinovicar@users.noreply.github.com>
2024-01-29 12:14:45 -0600

    color and component set up

---
+++ src/components/draw/CanvasControl.vue
@@ -0,0 +1,6 @@
+<template>
+    <button @click="$emit('undo')"> UNDO</button>"
+    <button @click="$emit('redo')"> REDO</button>"
+    <button @click="$emit('clearinit')"> CLEAR</button>"
+
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import {createPinia} from 'pinia'
 import router from './router'
 import './style.css'
 import App from './App.vue'
@@ -12,6 +13,9 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
     document.documentElement.classList.remove('dark')
 }
 
-createApp(App)
-.use(router)
+const app = createApp(App)
+const pinia =  createPinia()
+
+app.use(router)
+.use(pinia)
 .mount('#app')
